a6f74e544e9ec9e957e9898337ade2c6f89e6fca
Phil Chaplin <pchapl1@gmail.com>
2023-02-28 12:38:42 -0800

    added bootstrap and did some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,27 @@
 
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import "./Header.css";
 
 class Header extends React.Component {
 
     render() {
         return (
-            <div className='header'>
-                <h1>Gallery of Horns</h1>
 
-            </div>
+                <Navbar bg="light" expand="lg">
+                <Container>
+                  <Navbar.Brand href="#home">Gallery of Horns</Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    {/* <Nav className="me-auto">
+                      <Nav.Link href="#home">Home</Nav.Link>
+                      <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav> */}
+                  </Navbar.Collapse>
+                </Container>
+              </Navbar>
         )
     }
 }
